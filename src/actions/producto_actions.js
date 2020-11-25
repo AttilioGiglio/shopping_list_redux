68d548db4a1e915db74cab8b546484275984433b
attiliogiglio <attilio.g93@gmail.com>
2020-11-25 13:11:15 -0300

    agregar un nuevo producto al state

--- src/actions/producto_actions.js
+++ src/actions/producto_actions.js
@@ -7,7 +7,34 @@ import {
 //  Crear nuevos productos
 
 export function crearNuevoProductoAction(producto){
-    return () => {
-        console.log(producto)
+    return (dispatch) => {
+        // intento agregar un producto, si se puede cambia el estado a true
+        dispatch( agregarProducto() );
+        try {
+            //  se agrega el producto
+            dispatch( agregarProductoExito() );
+        }catch(error){
+            // no se pudo agregar el producto
+            dispatch( agregarProductoError(true) );
+        }
     }
 }
+
+const agregarProducto = () => ({
+    type: AGREGAR_PRODUCTO,
+    payload: true
+});
+
+// creo la action de agregar y guardar un producto en la base de datos
+const agregarProductoExito = producto => ({
+    //  para identificar la accion que se quiere ejecutar
+    type: AGREGAR_PRODUCTO_EXITO,
+    // para realizar la modificacion
+    payload: producto
+})
+
+// si hay un error
+const agregarProductoError = estado => ({
+    type: AGREGAR_PRODUCTO_ERROR,
+    payload: estado
+})
