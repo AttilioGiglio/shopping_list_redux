21aa7093c70dd5c16e64be63279a4c5f759b5154
attiliogiglio <attilio.g93@gmail.com>
2020-11-25 17:01:10 -0300

    create action to delete

--- src/components/producto.js
+++ src/components/producto.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { borrarProductoAction } from '../actions/producto_actions'
 
 const Producto = ({producto}) => {
     const { nombre, precio, id } = producto;
+    const dispatch = useDispatch();
+
+    // Confirmar si desea eliminar
+    const confirmarEliminarProducto = id => {
+
+        // preguntar al usuario
+
+        // pasarlo al action
+        dispatch( borrarProductoAction(id) )
+
+    }
+
     return (
         <tr>
             <td>{nombre}</td>
             <td><span className='font-weight-bold'>{precio}</span></td>
             <td className='acciones'>
             <Link to={`/productos/editar/${id}`} className='btn btn-primary mr-2' >Editar</Link>
-            <button type='button' className='btn btn-danger'> Eliminar </button>
+            <button onClick={()=>confirmarEliminarProducto(id)} type='button' className='btn btn-danger'> Eliminar </button>
             </td>
         </tr>
     )
--- src/components/productos.js
+++ src/components/productos.js
@@ -11,18 +11,20 @@ const Productos = () => {
     useEffect(() => {
         
         // consultar la API
-
         const cargarProductos = () => dispatch( obtenerProductosAction() );
         cargarProductos();
         
     }, [])
 
     const productos = useSelector( state => state.productos.productos );
-    console.log(productos)
+    const error = useSelector( state => state.productos.error );
+    const cargando = useSelector(state => state.productos.loading);
 
     return (
         <Fragment>
        <h2 className='text-center my-5'>Listado de Productos</h2>
+       { error ? <p className='font-weight-bold alert alert-danger text-center mt-4'>Hubo un error</p> : null}
+       { error ? <p className='text-center'>Cargando...</p> : null}
        <table className='table table-striped'>
        <thead className='bg-primary table-dark'>
         <tr>
@@ -32,14 +34,18 @@ const Productos = () => {
         </tr>
        </thead>
        <tbody>
-       {productos.length === 0 ? 'No hay productos' : (
+       {productos.length === 0 
+       ? 'No hay productos' 
+       : 
+       (
            productos.map(producto => (
                <Producto 
                key={producto.id}
                producto={producto}
                />
-           ))
-       )}
+            ))
+       )
+       }
        </tbody>
        </table>     
         </Fragment>
